2f207bbf791c3f2f3b462f12f0405c5f27973e48
shermanip <s.ip@warwick.ac.uk>
2018-08-09 08:39:54 +0100

    Added graph distribution, this fixed bug in pervious commit.

---
+++ src/uk/ac/warwick/sip/mcmc/GraphDistribution.java
@@ -0,0 +1,53 @@
+package uk.ac.warwick.sip.mcmc;
+
+import org.ejml.simple.SimpleMatrix;
+import seed.minerva.GraphicalModel;
+
+/**CLASS: GRAPH DISTRIBUTION
+ * Evaluate the pdf using the MINERVA GraphicalModel object
+ */
+public class GraphDistribution extends TargetDistribution {
+	
+	//MINERVA object which can evaluate the log pdf
+	protected GraphicalModel graph;
+	
+	/**CONSTRUCTOR
+	 * Construct the target pdf using the minerva GraphicalModel object
+	 * @param graph GraphicalModel which can evaluate the log pdf
+	 */
+	public GraphDistribution(GraphicalModel graph) {
+		super(graph.getFreeParameters().length);
+		this.graph = graph;
+	}
+	
+	/**IMPLEMENTED: GET PDF
+	 * @param x Where to evaluate the pdf, column vector
+	 * @return The evaluation of the pdf at x up to a constant
+	 */
+	@Override
+	public double getPdf(SimpleMatrix x) {
+		return Math.exp(-this.getPotential(x));
+	}
+	
+	/**IMPLEMENTED: GET POTENTIAL
+	 * @param x Where to evaluate the potential
+	 * @return The evaluation of the potential at x + some constant
+	 */
+	@Override
+	public double getPotential(SimpleMatrix x) {
+		this.graph.setFreeParameters(x.getDDRM().getData());
+		return -this.graph.logPdf();
+	}
+	
+	/**IMPLEMENTED: GET D POTENTIAL
+	 * THROWS EXCPETION
+	 * TODO Implemented numerical differentiation
+	 * @param x Where to evaluate the potential gradient
+	 * @return The evaluation of the potential gradient at x
+	 */
+	@Override
+	public SimpleMatrix getDPotential(SimpleMatrix x) {
+		throw new RuntimeException();
+	}
+
+}
